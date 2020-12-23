191487ed1ca4e37bb526bc579b5d63993f20abb2
Jon Snow <885479297@qq.com>
2020-12-23 19:03:04 +0800

    css

--- src/myElement/index.js
+++ src/myElement/index.js
@@ -1,8 +1,13 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 
 export default class MyElement extends LitElement {
     static name() {
         return 'my-element'
+    }
+    static get styles() {
+        return css`
+            div { color: red;}
+        `
     }
     render() {
         return html`
